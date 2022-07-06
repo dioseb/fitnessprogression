d6a2765157024636686eb77fd2e9724f003bd43a
D_IPH\Smedel <sebastien.medel@wiv-isp.be>
2022-07-06 19:23:50 +0200

    config.js

--- app/api/config/config.js
+++ app/api/config/config.js
@@ -1,2 +1,3 @@
 //export const USER_API_ENDPOINT = 'http://10.5.118.95:3000/api/users';
-export const USER_API_ENDPOINT = 'http://192.168.0.5:3000/api/users';
+//export const USER_API_ENDPOINT = 'http://192.168.0.5:3000/api/users';
+export const USER_API_ENDPOINT = "http://localhost:3000/api/users";
